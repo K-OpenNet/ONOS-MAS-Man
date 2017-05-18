835e7b27117e5334a7981d47d11ab610fafab90d
woojoong <woojoong@postech.ac.kr>
2017-05-18 15:01:23 +0900

    write configurationcontroller.java

--- src/main/java/kr/postech/monet/controller/ConfigurationController.java
+++ src/main/java/kr/postech/monet/controller/ConfigurationController.java
@@ -1,7 +1,16 @@
 package kr.postech.monet.controller;
 
+import kr.postech.monet.config.GeneralConf;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by woojoong on 2017-05-18.
  */
 public class ConfigurationController {
+    @RequestMapping("/configuration")
+    public String configuration(Model model) {
+        model.addAttribute("confList", GeneralConf.siteConfPoolList.getSiteBeans());
+        return "configuration";
+    }
 }
