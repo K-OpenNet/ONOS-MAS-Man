3e1f1243fa477900486b2e13cd4f1d9942765b02
woojoong <woojoong@postech.ac.kr>
2017-05-18 14:48:26 +0900

    write numswitchbean.java

--- src/main/java/kr/postech/monet/core/database/bean/NumSwitchesBean.java
+++ src/main/java/kr/postech/monet/core/database/bean/NumSwitchesBean.java
@@ -4,4 +4,27 @@ package kr.postech.monet.core.database.bean;
  * Created by woojoong on 2017-05-18.
  */
 public class NumSwitchesBean {
+    private String time;
+    private int numSwitches;
+
+    public NumSwitchesBean(String time, int numSwitches) {
+        this.time = time;
+        this.numSwitches = numSwitches;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getNumSwitches() {
+        return numSwitches;
+    }
+
+    public void setNumSwitches(int numSwitches) {
+        this.numSwitches = numSwitches;
+    }
 }
