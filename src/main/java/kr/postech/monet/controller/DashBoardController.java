2034696fd723b5dd8d3681aa5053b100605cfe68
woojoong <woojoong@postech.ac.kr>
2017-05-18 15:02:23 +0900

    write dashboardcontroller.java

--- src/main/java/kr/postech/monet/controller/DashBoardController.java
+++ src/main/java/kr/postech/monet/controller/DashBoardController.java
@@ -1,7 +1,15 @@
 package kr.postech.monet.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by woojoong on 2017-05-18.
  */
 public class DashBoardController {
+    @RequestMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAttribute("dashboard", "hello");
+        return "dashboard";
+    }
 }
