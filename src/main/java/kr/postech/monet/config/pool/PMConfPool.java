586c447daf0555f2667e80e3c95f693611a6d220
woojoong <woojoong@postech.ac.kr>
2017-05-18 14:24:47 +0900

    write pmconfpool.java

--- src/main/java/kr/postech/monet/config/pool/PMConfPool.java
+++ src/main/java/kr/postech/monet/config/pool/PMConfPool.java
@@ -1,7 +1,25 @@
 package kr.postech.monet.config.pool;
 
+import kr.postech.monet.config.bean.PMBean;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * Created by woojoong on 2017-05-18.
  */
 public class PMConfPool {
+    private List<PMBean> pmBeans;
+
+    public PMConfPool() {
+        pmBeans = new CopyOnWriteArrayList<PMBean>();
+    }
+
+    public List<PMBean> getPmBeans() {
+        return pmBeans;
+    }
+
+    public void setPmBeans(List<PMBean> pmBeans) {
+        this.pmBeans = pmBeans;
+    }
 }
