d012bca447c8a3fa0d7887e8e5915f39e0fe0159
woojoong <woojoong@postech.ac.kr>
2017-05-18 14:58:22 +0900

    write getting functions to get switches and masterroleswitches in gettingtopology.java

--- src/main/java/kr/postech/monet/core/monitor/GettingTopology.java
+++ src/main/java/kr/postech/monet/core/monitor/GettingTopology.java
@@ -1,7 +1,37 @@
 package kr.postech.monet.core.monitor;
 
+import kr.postech.monet.config.bean.SWBean;
+import kr.postech.monet.config.bean.VMBean;
+import kr.postech.monet.core.parser.RESTParser;
+import kr.postech.monet.utils.RESTConnectionUtil;
+
+import java.util.List;
+
 /**
  * Created by woojoong on 2017-05-18.
  */
 public class GettingTopology {
+    public GettingTopology() {
+    }
+
+    public List<SWBean> getSwitches(VMBean sourceVM) {
+
+        RESTConnectionUtil restConn = new RESTConnectionUtil();
+        String cmdURL = "http://" + sourceVM.getAccessIPAddress() + ":" +
+                sourceVM.getAccessHTTPPort() + "/onos/v1/devices";
+        String restraw = restConn.getRESTToSingleVM(sourceVM, cmdURL);
+        RESTParser parser = new RESTParser();
+        return parser.parseGetSwitches(restraw);
+    }
+
+    public List<String> getMasterRoleSwitches(VMBean sourceVM) {
+
+        RESTConnectionUtil restConn = new RESTConnectionUtil();
+        String cmdURL = "http://" + sourceVM.getAccessIPAddress() + ":" + sourceVM.getAccessHTTPPort()
+                + "/onos/v1/mastership/" + sourceVM.getIpAddress() + "/device";
+        String restraw = restConn.getRESTToSingleVM(sourceVM, cmdURL);
+        RESTParser parser = new RESTParser();
+
+        return parser.parseGetMasterRoleSwitches(restraw);
+    }
 }
