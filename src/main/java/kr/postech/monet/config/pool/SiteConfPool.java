9fde692605108b7ea422d94733249f0a1ad72b05
woojoong <woojoong@postech.ac.kr>
2017-05-18 14:25:31 +0900

    write siteconfpool.java

--- src/main/java/kr/postech/monet/config/pool/SiteConfPool.java
+++ src/main/java/kr/postech/monet/config/pool/SiteConfPool.java
@@ -1,7 +1,25 @@
 package kr.postech.monet.config.pool;
 
+import kr.postech.monet.config.bean.SiteBean;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * Created by woojoong on 2017-05-18.
  */
 public class SiteConfPool {
+    private List<SiteBean> siteBeans;
+
+    public SiteConfPool() {
+        siteBeans = new CopyOnWriteArrayList<SiteBean>();
+    }
+
+    public List<SiteBean> getSiteBeans() {
+        return siteBeans;
+    }
+
+    public void setSiteBeans(List<SiteBean> siteBeans) {
+        this.siteBeans = siteBeans;
+    }
 }
