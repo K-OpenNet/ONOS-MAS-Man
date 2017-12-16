4b834257c106367e381a50aa51c9a63d9a40fb05
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-16 15:30:24 -0800

    define interface functions in connection classes as a template

--- src/Utils/Connection/RESTConnection.java
+++ src/Utils/Connection/RESTConnection.java
@@ -1,4 +1,13 @@
 package Utils.Connection;
 
+import Beans.Bean;
+
 public class RESTConnection extends AbstractConnection implements Connection {
+    public RESTConnection() {
+    }
+
+    @Override
+    public String sendCommand(Bean targetMachine) {
+        return null;
+    }
 }
--- src/Utils/Connection/SSHConnection.java
+++ src/Utils/Connection/SSHConnection.java
@@ -1,4 +1,13 @@
 package Utils.Connection;
 
+import Beans.Bean;
+
 public class SSHConnection extends AbstractConnection implements Connection {
+    public SSHConnection() {
+    }
+
+    @Override
+    public String sendCommand(Bean targetMachine) {
+        return null;
+    }
 }
