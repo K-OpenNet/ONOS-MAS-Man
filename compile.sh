e4c0d4dd50c13e9f2546731d21fe55ee24ef3fa6
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-20 13:24:42 -0800

    Add lines in shell scripts to make sure bash environment

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 rm -rf out
 mkdir out
 javac -cp $(pwd)/src:$(pwd)/lib/* -d out $(find . -name *.java)
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+
 ./compile.sh
+
 pushd $(pwd)/out
 java -cp .:../lib/* Controller.Controller
 popd
