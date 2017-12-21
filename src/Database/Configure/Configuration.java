8f4c2f277104092c852a35ab0bdaabf1bf99a0b3
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-20 18:54:27 -0800

    define configuration class which includes Controllers' and PMs' information

--- src/Database/Configure/Configuration.java
+++ src/Database/Configure/Configuration.java
@@ -1,5 +1,11 @@
 package Database.Configure;
 
+import Beans.ControllerBean;
+import Beans.PMBean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Configuration {
     private static Configuration ourInstance = new Configuration();
 
@@ -8,5 +14,37 @@ public class Configuration {
     }
 
     private Configuration() {
+        controllers = new ArrayList<>();
+        pms = new ArrayList<>();
+        relationships = new HashMap<>();
+    }
+
+    private ArrayList<ControllerBean> controllers;
+    private ArrayList<PMBean> pms;
+    private HashMap<PMBean, ArrayList<ControllerBean>> relationships;
+
+
+    public ArrayList<ControllerBean> getControllers() {
+        return controllers;
+    }
+
+    public void setControllers(ArrayList<ControllerBean> controllers) {
+        this.controllers = controllers;
+    }
+
+    public ArrayList<PMBean> getPms() {
+        return pms;
+    }
+
+    public void setPms(ArrayList<PMBean> pms) {
+        this.pms = pms;
+    }
+
+    public HashMap<PMBean, ArrayList<ControllerBean>> getRelationships() {
+        return relationships;
+    }
+
+    public void setRelationships(HashMap<PMBean, ArrayList<ControllerBean>> relationships) {
+        this.relationships = relationships;
     }
 }
