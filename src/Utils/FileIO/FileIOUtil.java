0f8a6c6234349954468d214e2791a6d8c9e9b21b
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-20 18:42:08 -0800

    Make FileIO class and reading function from file

---
+++ src/Utils/FileIO/FileIOUtil.java
@@ -0,0 +1,38 @@
+package Utils.FileIO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileIOUtil {
+    public FileIOUtil() {
+    }
+
+    public String getRawFileContents (String path) {
+        String results = null;
+
+        try {
+            File file = new File(path);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            char[] buffer = new char[(int)file.length()];
+            br.read(buffer);
+
+            results = String.valueOf(buffer);
+
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot open the file: " + path);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Cannot close the file and its buffer/reader: " + path);
+            e.printStackTrace();
+        }
+
+        return results;
+    }
+}
