d489419ccbcf5fe91e292f70bc9960e749a00422
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-21 10:46:10 -0800

    Add SSH assignment codes for PMs and Controllers

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,7 +1,10 @@
 package Controller;
 
+import Beans.ControllerBean;
+import Beans.PMBean;
 import Database.Configure.Configuration;
 import UserInterface.CLI.CommandLine;
+import Utils.Connection.SSHConnection;
 import Utils.FileIO.FileIOUtil;
 import Utils.Parser.JsonParser;
 
@@ -17,7 +20,7 @@ public class Controller {
     }
 
     public static void init() {
-        System.out.println("Initialization now...");
+        System.out.println("** Initialization now...");
 
         FileIOUtil fileIO = new FileIOUtil();
         JsonParser parser = new JsonParser();
@@ -28,8 +31,30 @@ public class Controller {
             System.exit(0);
         }
 
+        System.out.println("- configuration file has been applied on this system, successfully");
+
         Configuration config = Configuration.getInstance();
-        System.out.println("Initialization has been finished.");
+        SSHConnection conn = new SSHConnection();
+        // assign SSH session for each PM
+        for (PMBean pm : config.getPms()) {
+            conn.assignUserSession(pm);
+            conn.assignRootSession(pm);
+
+            System.out.println("The SSH sessions for the PM, " + pm.getBeanKey() + ", has been set");
+
+        }
+
+        // assign SSH session for each Controller
+        for (ControllerBean controller : config.getControllers()) {
+            conn.assignUserSession(controller);
+            conn.assignRootSession(controller);
+
+            System.out.println("The SSH sessions for the Controller, " + controller.getBeanKey() + ", has been set");
+        }
+
+        // ToDo: make CPU bitmap for controller
+
+        System.out.println("** Initialization has been finished.");
     }
 
     public static void main (String[] args) {
