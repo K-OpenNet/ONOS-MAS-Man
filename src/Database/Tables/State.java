557805c1ed3e365b801c7d610b8177f35636dab0
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-29 23:26:29 -0800

    Add ControlPlaneTuple functions

--- src/Database/Tables/State.java
+++ src/Database/Tables/State.java
@@ -3,6 +3,7 @@ package Database.Tables;
 import Beans.ControllerBean;
 import Beans.SwitchBean;
 import Database.Tuples.ComputingResourceTuple;
+import Database.Tuples.ControlPlaneTuple;
 import Database.Tuples.MastershipTuple;
 
 import java.util.HashMap;
@@ -11,11 +12,15 @@ public class State {
 
     // Key: controllerKey, value: MastershipTuple
     private HashMap<String, MastershipTuple> mastershipTuples;
+    // Key: controllerKey, value: ComputingResourceTuple
     private HashMap<String, ComputingResourceTuple> computingResourceTuples;
+    // key: switchKey, value: ControlPlaneTuple
+    private HashMap<String, ControlPlaneTuple> controlPlaneTuples;
 
     public State() {
         mastershipTuples = new HashMap<>();
         computingResourceTuples = new HashMap<>();
+        controlPlaneTuples = new HashMap<>();
     }
 
     public void addMastershipTuple(ControllerBean controller, SwitchBean sw) {
@@ -38,6 +43,14 @@ public class State {
         computingResourceTuples.put(controller.getBeanKey(), computingResourceTuple);
     }
 
+    public void addControlPlaneTuple(SwitchBean sw, ControlPlaneTuple controlPlaneTuple) {
+        if(controlPlaneTuples.containsKey(sw.getBeanKey())) {
+            throw new ControlPlaneTupleSanityException();
+        }
+
+        controlPlaneTuples.put(sw.getBeanKey(), controlPlaneTuple);
+    }
+
     public HashMap<String, MastershipTuple> getMastershipTuples() {
         return mastershipTuples;
     }
@@ -52,6 +65,14 @@ public class State {
 
     public void setComputingResourceTuples(HashMap<String, ComputingResourceTuple> computingResourceTuples) {
         this.computingResourceTuples = computingResourceTuples;
+    }
+
+    public HashMap<String, ControlPlaneTuple> getControlPlaneTuples() {
+        return controlPlaneTuples;
+    }
+
+    public void setControlPlaneTuples(HashMap<String, ControlPlaneTuple> controlPlaneTuples) {
+        this.controlPlaneTuples = controlPlaneTuples;
     }
 }
 
@@ -71,6 +92,16 @@ class ComputingResourceTupleSanityException extends RuntimeException {
     }
 
     public ComputingResourceTupleSanityException(String message) {
+        super(message);
+    }
+}
+
+class ControlPlaneTupleSanityException extends RuntimeException {
+    public ControlPlaneTupleSanityException() {
+        super();
+    }
+
+    public ControlPlaneTupleSanityException(String message) {
         super(message);
     }
 }
--- src/Database/Tuples/ControlPlaneTuple.java
+++ src/Database/Tuples/ControlPlaneTuple.java
@@ -1,7 +1,85 @@
 package Database.Tuples;
 
+import org.projectfloodlight.openflow.protocol.OFType;
+
+import java.util.HashMap;
+
 public class ControlPlaneTuple extends AbstractTuple implements Tuple {
+
+    private HashMap<OFType, Long> controlTrafficResults;
+    private HashMap<OFType, Long> controlTrafficByteResults;
+
+    private HashMap<OFType, Long> controlTrafficRawResults;
+    private HashMap<OFType, Long> controlTrafficByteRawResults;
+
+    private HashMap<OFType, Long> outdatedControlTrafficResults;
+    private HashMap<OFType, Long> outdatedControlTrafficBytesResults;
+
     public ControlPlaneTuple() {
         tupleName = tupleType.CONTROLPLANETUPLE;
+
+        initHashMaps(controlTrafficResults);
+        initHashMaps(controlTrafficByteResults);
+        initHashMaps(controlTrafficRawResults);
+        initHashMaps(controlTrafficByteRawResults);
+        initHashMaps(outdatedControlTrafficResults);
+        initHashMaps(outdatedControlTrafficBytesResults);
+
+    }
+
+    public void initHashMaps(HashMap<OFType, Long> maps) {
+        maps = new HashMap<>();
+
+        for (OFType type : OFType.values()) {
+            maps.put(type, (long) 0);
+        }
+    }
+
+    public HashMap<OFType, Long> getControlTrafficResults() {
+        return controlTrafficResults;
+    }
+
+    public void setControlTrafficResults(HashMap<OFType, Long> controlTrafficResults) {
+        this.controlTrafficResults = controlTrafficResults;
+    }
+
+    public HashMap<OFType, Long> getControlTrafficByteResults() {
+        return controlTrafficByteResults;
+    }
+
+    public void setControlTrafficByteResults(HashMap<OFType, Long> controlTrafficByteResults) {
+        this.controlTrafficByteResults = controlTrafficByteResults;
+    }
+
+    public HashMap<OFType, Long> getControlTrafficRawResults() {
+        return controlTrafficRawResults;
+    }
+
+    public void setControlTrafficRawResults(HashMap<OFType, Long> controlTrafficRawResults) {
+        this.controlTrafficRawResults = controlTrafficRawResults;
+    }
+
+    public HashMap<OFType, Long> getControlTrafficByteRawResults() {
+        return controlTrafficByteRawResults;
+    }
+
+    public void setControlTrafficByteRawResults(HashMap<OFType, Long> controlTrafficByteRawResults) {
+        this.controlTrafficByteRawResults = controlTrafficByteRawResults;
+    }
+
+    public HashMap<OFType, Long> getOutdatedControlTrafficResults() {
+        return outdatedControlTrafficResults;
+    }
+
+    public void setOutdatedControlTrafficResults(HashMap<OFType, Long> outdatedControlTrafficResults) {
+        this.outdatedControlTrafficResults = outdatedControlTrafficResults;
+    }
+
+    public HashMap<OFType, Long> getOutdatedControlTrafficBytesResults() {
+        return outdatedControlTrafficBytesResults;
+    }
+
+    public void setOutdatedControlTrafficBytesResults(HashMap<OFType, Long> outdatedControlTrafficBytesResults) {
+        this.outdatedControlTrafficBytesResults = outdatedControlTrafficBytesResults;
     }
 }
