709a4586a7214bd4a14a75c61ac882510bc6e27a
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-29 23:29:58 -0800

    make database which is defined as Static

--- src/Database/Tables/Database.java
+++ src/Database/Tables/Database.java
@@ -1,12 +1,35 @@
 package Database.Tables;
 
+import java.util.ArrayList;
+
 public class Database {
     private static Database ourInstance = new Database();
+    private static ArrayList<State> database = new ArrayList<>();
 
     public static Database getInstance() {
         return ourInstance;
     }
 
     private Database() {
+    }
+
+    public State getAllTuples(int timeIndex) {
+        return database.get(timeIndex);
+    }
+
+    public static Database getOurInstance() {
+        return ourInstance;
+    }
+
+    public static void setOurInstance(Database ourInstance) {
+        Database.ourInstance = ourInstance;
+    }
+
+    public static ArrayList<State> getDatabase() {
+        return database;
+    }
+
+    public static void setDatabase(ArrayList<State> database) {
+        Database.database = database;
     }
 }
