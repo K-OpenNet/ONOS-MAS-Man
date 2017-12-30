ddc1c048b0909d68afec321e4977ddb4bf813841
Woojoong Kim <woojoong.m.kim@gmail.com>
2017-12-29 22:34:54 -0800

    Add MastershipTuple functions

--- src/Database/Tables/State.java
+++ src/Database/Tables/State.java
@@ -1,4 +1,47 @@
 package Database.Tables;
 
+import Beans.ControllerBean;
+import Beans.SwitchBean;
+import Database.Tuples.MastershipTuple;
+
+import java.util.HashMap;
+
 public class State {
+
+    // Key: controllerKey, value: MastershipTuple
+    private HashMap<String, MastershipTuple> mastershipTuples;
+
+    public State() {
+        mastershipTuples = new HashMap<>();
+    }
+
+    public void addMastershipTuple(ControllerBean controller, SwitchBean sw) {
+        if(!mastershipTuples.containsKey(controller.getBeanKey())) {
+            mastershipTuples.put(controller.getBeanKey(), new MastershipTuple());
+        }
+
+        if(mastershipTuples.get(controller.getBeanKey()).hasSwitch(sw.getBeanKey())) {
+            throw new MastershipTupleSanityException();
+        }
+
+        mastershipTuples.get(controller.getBeanKey()).addSwitch(sw.getBeanKey());
+    }
+
+    public HashMap<String, MastershipTuple> getMastershipTuples() {
+        return mastershipTuples;
+    }
+
+    public void setMastershipTuples(HashMap<String, MastershipTuple> mastershipTuples) {
+        this.mastershipTuples = mastershipTuples;
+    }
+}
+
+class MastershipTupleSanityException extends RuntimeException {
+    public MastershipTupleSanityException() {
+        super();
+    }
+
+    public MastershipTupleSanityException(String message) {
+        super(message);
+    }
 }
--- src/Database/Tuples/MastershipTuple.java
+++ src/Database/Tuples/MastershipTuple.java
@@ -1,7 +1,33 @@
 package Database.Tuples;
 
+import java.util.ArrayList;
+
 public class MastershipTuple extends AbstractTuple implements Tuple {
+
+    private ArrayList<String> switchList;
+
     public MastershipTuple() {
         tupleName = tupleType.MASTERSHIPTUPLE;
+        switchList = new ArrayList<>();
+    }
+
+    public boolean hasSwitch(String switchKey) {
+        return switchList.contains(switchKey);
+    }
+
+    public void addSwitch(String switchKey) {
+        switchList.add(switchKey);
+    }
+
+    public void delSwitch(String switchKey) {
+        switchList.remove(switchKey);
+    }
+
+    public ArrayList<String> getSwitchList() {
+        return switchList;
+    }
+
+    public void setSwitchList(ArrayList<String> switchList) {
+        this.switchList = switchList;
     }
 }
