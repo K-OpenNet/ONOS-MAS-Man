40dd4da08e88b0ee7cda442121be4bba46c85873
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-02 15:58:50 -0800

    Scaffolding ControlPlane Monitor

--- src/Monitor/ControlPlaneMonitor.java
+++ src/Monitor/ControlPlaneMonitor.java
@@ -1,7 +1,42 @@
 package Monitor;
 
+import Beans.ControllerBean;
+import Database.Tables.Database;
+import Database.Tuples.ControlPlaneTuple;
+import Utils.Connection.RESTConnection;
+import Utils.Parser.JsonParser;
+import org.projectfloodlight.openflow.protocol.OFType;
+
+import java.util.HashMap;
+
+import static Database.Configure.Configuration.RESTURL_CPMESSAGES;
+
 public class ControlPlaneMonitor extends AbstractMonitor implements Monitor {
     public ControlPlaneMonitor() {
         monitorName = monitorType.CONTROLPLANEMONITOR;
+    }
+
+    public String monitorRawControlPlane(ControllerBean controller) {
+        RESTConnection restConn = new RESTConnection();
+
+        return restConn.sendCommandToUser(controller, RESTURL_CPMESSAGES);
+    }
+
+    public HashMap<String, ControlPlaneTuple> monitorControlPlaneInit() {
+
+        JsonParser parser = new JsonParser();
+        HashMap<String, ControlPlaneTuple> results = new HashMap<>();
+
+        return results;
+    }
+
+    public HashMap<String, ControlPlaneTuple> monitorControlPlane(int timeIndex) {
+
+        Database db = Database.getInstance();
+
+        HashMap<String, ControlPlaneTuple> prevResults = db.getAllTuples(timeIndex).getControlPlaneTuples();
+        HashMap<String, ControlPlaneTuple> currentResults = monitorControlPlaneInit();
+
+        return currentResults;
     }
 }
