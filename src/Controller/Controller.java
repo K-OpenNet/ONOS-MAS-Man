9ef44ef310f6b254115c5b49dc359e7e6af95a49
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-02 21:55:04 -0800

    Change init function based on thread for speed up

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -9,6 +9,8 @@ import Utils.Connection.SSHConnection;
 import Utils.FileIO.FileIOUtil;
 import Utils.Parser.JsonParser;
 
+import java.util.ArrayList;
+
 public class Controller {
     private static Controller ourInstance = new Controller();
 
@@ -24,6 +26,8 @@ public class Controller {
     public static void init() {
         System.out.println("** Initialization now...");
 
+        ArrayList<Thread> threads = new ArrayList<>();
+
         FileIOUtil fileIO = new FileIOUtil();
         JsonParser parser = new JsonParser();
         try {
@@ -36,23 +40,28 @@ public class Controller {
         System.out.println("- configuration file has been applied on this system, successfully");
 
         Configuration config = Configuration.getInstance();
-        SSHConnection conn = new SSHConnection();
         // assign SSH session for each PM
         for (PMBean pm : config.getPms()) {
-            conn.assignUserSession(pm);
-            conn.assignRootSession(pm);
-
-            System.out.println("The SSH sessions for the PM, " + pm.getBeanKey() + ", has been set");
-
+            ThreadPMSSHSessionAssignment pmRunnableObj = new ThreadPMSSHSessionAssignment(pm);
+            Thread thread = new Thread(pmRunnableObj);
+            threads.add(thread);
+            thread.start();
         }
-        ComputingResourceMonitor mon = new ComputingResourceMonitor();
+
         // assign SSH session for each Controller as well as assign CPUBitMap for each Controller
         for (ControllerBean controller : config.getControllers()) {
-            conn.assignUserSession(controller);
-            conn.assignRootSession(controller);
+            ThreadControllerSSHSessionAssignment controllerRunnableObj = new ThreadControllerSSHSessionAssignment(controller);
+            Thread thread = new Thread(controllerRunnableObj);
+            threads.add(thread);
+            thread.start();
+        }
 
-            System.out.println("The SSH sessions for the Controller, " + controller.getBeanKey() + ", has been set");
-            mon.monitorCPUBitMap(controller);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         System.out.println("** Initialization has been finished.");
@@ -63,4 +72,44 @@ public class Controller {
         cli.startCLI();
     }
 
+}
+
+class ThreadPMSSHSessionAssignment implements Runnable {
+
+    private PMBean pm;
+
+    public ThreadPMSSHSessionAssignment(PMBean pm) {
+        this.pm = pm;
+    }
+
+    @Override
+    public void run() {
+        SSHConnection conn = new SSHConnection();
+        conn.assignUserSession(pm);
+        conn.assignRootSession(pm);
+
+        System.out.println("The SSH sessions for the PM, " + pm.getBeanKey() + ", has been set");
+
+    }
+}
+
+class ThreadControllerSSHSessionAssignment implements Runnable {
+
+    private ControllerBean controller;
+
+    public ThreadControllerSSHSessionAssignment(ControllerBean controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void run() {
+        SSHConnection conn = new SSHConnection();
+        ComputingResourceMonitor mon = new ComputingResourceMonitor();
+
+        conn.assignUserSession(controller);
+        conn.assignRootSession(controller);
+
+        System.out.println("The SSH sessions for the Controller, " + controller.getBeanKey() + ", has been set");
+        mon.monitorCPUBitMap(controller);
+    }
 }
