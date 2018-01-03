de49bea72db2292233e520125ab4ffd039459d4f
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-02 16:18:02 -0800

    Implement a monitoring function when operating in runtime

--- src/Monitor/ControlPlaneMonitor.java
+++ src/Monitor/ControlPlaneMonitor.java
@@ -32,11 +32,68 @@ public class ControlPlaneMonitor extends AbstractMonitor implements Monitor {
 
     public HashMap<String, ControlPlaneTuple> monitorControlPlane(int timeIndex) {
 
+        // TODO: change to Thread
+        if (timeIndex == 0) {
+            System.out.println("Not available this function when timeIndex == 0");
+            throw new WrongTimeIndexException();
+        }
+
         Database db = Database.getInstance();
 
         HashMap<String, ControlPlaneTuple> prevResults = db.getAllTuples(timeIndex).getControlPlaneTuples();
         HashMap<String, ControlPlaneTuple> currentResults = monitorControlPlaneInit();
 
+        for (String dpid : currentResults.keySet()) {
+
+            if (!prevResults.keySet().contains(dpid)) {
+                continue;
+            }
+
+            ControlPlaneTuple tmpTuple = currentResults.get(dpid);
+            ControlPlaneTuple prevTuple = prevResults.get(dpid);
+            for (OFType type : OFType.values()) {
+                long currentControlTrafficRawResult = tmpTuple.getControlTrafficRawResults().get(type)
+                        + tmpTuple.getOutdatedControlTrafficResults().get(type);
+                long currentControlTrafficByteRawResults = tmpTuple.getControlTrafficByteRawResults().get(type)
+                        + tmpTuple.getOutdatedControlTrafficBytesResults().get(type);
+                long prevControlTrafficRawResult = prevTuple.getControlTrafficRawResults().get(type)
+                        + prevTuple.getOutdatedControlTrafficResults().get(type);
+                long prevControlTrafficByteRawResults = prevTuple.getControlTrafficByteRawResults().get(type)
+                        + prevTuple.getOutdatedControlTrafficBytesResults().get(type);
+
+                long tmpControlTrafficResult = currentControlTrafficRawResult - prevControlTrafficRawResult;
+                long tmpControlTrafficByteResult = currentControlTrafficByteRawResults - prevControlTrafficByteRawResults;
+
+                if (tmpControlTrafficByteResult < 0 || tmpControlTrafficResult < 0) {
+                    throw new ControlPlaneMonitoringResultSanityException();
+                }
+
+                tmpTuple.getControlTrafficResults().replace(type, tmpControlTrafficResult);
+                tmpTuple.getControlTrafficByteResults().replace(type, tmpControlTrafficByteResult);
+            }
+
+        }
+
         return currentResults;
+    }
+}
+
+class WrongTimeIndexException extends RuntimeException {
+    public WrongTimeIndexException() {
+        super();
+    }
+
+    public WrongTimeIndexException(String message) {
+        super(message);
+    }
+}
+
+class ControlPlaneMonitoringResultSanityException extends RuntimeException {
+    public ControlPlaneMonitoringResultSanityException() {
+        super();
+    }
+
+    public ControlPlaneMonitoringResultSanityException(String message) {
+        super(message);
     }
 }
