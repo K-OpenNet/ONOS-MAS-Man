dfa06ec5b1f4b12a459ad16bec1dd05679cdd6e6
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-08 09:10:36 -0800

    insert test code

--- src/Monitor/ComputingResourceMonitor.java
+++ src/Monitor/ComputingResourceMonitor.java
@@ -46,10 +46,10 @@ public class ComputingResourceMonitor extends AbstractMonitor implements Monitor
         String cmd  = CMD_CPU_BITMAP_TEMPLATE;
 
         String tmpResults = sshConn.sendCommandToRoot(controllerBean, cmd);
+        System.out.println(controllerBean.getBeanKey() + ": " + tmpResults);
 
         while (tmpResults == null) {
             tmpResults = sshConn.sendCommandToRoot(controllerBean, cmd);
-            System.out.println(controllerBean.getBeanKey() + ": " + tmpResults);
         }
 
         String results = tmpResults.split("\\s+")[3];
