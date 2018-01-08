0feee5d1103e0767f327fa3031c3e388d161db67
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-08 15:12:48 -0800

    revert bug fix which removes new line character

--- src/Utils/Connection/SSHConnection.java
+++ src/Utils/Connection/SSHConnection.java
@@ -33,7 +33,7 @@ public class SSHConnection extends AbstractConnection implements Connection {
             String tmpLine;
             results = br.readLine() + "\n";
             while ((tmpLine = br.readLine()) != null) {
-                results = results + tmpLine;
+                results = results + tmpLine + "\n";
             }
 
             channel.disconnect();
@@ -65,7 +65,7 @@ public class SSHConnection extends AbstractConnection implements Connection {
             String tmpLine;
             results = br.readLine() + "\n";
             while ((tmpLine = br.readLine()) != null) {
-                results = results + tmpLine;
+                results = results + tmpLine + "\n";
             }
 
             channel.disconnect();
