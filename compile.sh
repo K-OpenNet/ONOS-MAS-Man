2cf8c423102f6e8ef8a0cdfb732138efc70825a2
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-17 21:27:57 -0800

    enhance shell script to automatically copy result files to a given dir

--- compile.sh
+++ compile.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+RESULT_DIR=$1
+EXP_INDEX=$2
+
+if [ ! -e $RESULT_DIR/$EXP_INDEX ]
+then
+    mkdir $RESULT_DIR
+fi
+
+if [ -e ./*.csv ]
+then
+    cp ./*.csv $RESULT_DIR/$EXP_INDEX
+elif [ -e ./out/*.csv ]
+then
+    cp ./out/*.csv $RESULT_DIR/$EXP_INDEX
+fi
+
 rm -rf out
 mkdir out
 cp config.json out/config.json
--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-./compile.sh
+if [ $0 -lt 2 ]
+then
+    echo "miss some parameters"
+fi
+
+./compile.sh $1 $2
 
 pushd $(pwd)/out
 java -cp .:../lib/* Controller.Controller
