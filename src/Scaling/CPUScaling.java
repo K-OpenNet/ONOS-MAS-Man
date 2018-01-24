2a5e08a5ad75025c3494d6f0cfa3990a16e7edd0
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-01-24 00:11:28 -0800

    [CARD-28] Make scaffolds to inc/dec virtual CPUs functions

--- src/Scaling/CPUScaling.java
+++ src/Scaling/CPUScaling.java
@@ -1,9 +1,51 @@
 package Scaling;
 
+import Beans.ControllerBean;
+
 import static Scaling.Scaling.scalingType.CPUSCALING;
 
 public class CPUScaling extends AbstractScaling implements Scaling {
     public CPUScaling() {
         scalingName = CPUSCALING;
+    }
+
+    public void incVirtualCPU (ControllerBean controller) {
+        int index = getCPUIndexForInc(controller);
+        if (index != -1) {
+            enableSpecificVirtualCPU(controller, index);
+        }
+    }
+
+    public void decVirtualCPU (ControllerBean controller) {
+        int index = getCPUIndexForDec(controller);
+        if (index != -1) {
+            disableSpecificVirtualCPU(controller, index);
+        }
+    }
+
+    public void enableSpecificVirtualCPU (ControllerBean controller, int cpuNumber) {
+
+    }
+
+    public void disableSpecificVirtualCPU (ControllerBean controller, int cpuNumber) {
+
+    }
+
+    public int getCPUIndexForInc (ControllerBean controller) {
+
+        for (int index = 0; index < controller.getCpuBitmap().length; index++) {
+
+        }
+
+        return -1;
+    }
+
+    public int getCPUIndexForDec (ControllerBean controller) {
+
+        for (int index = controller.getCpuBitmap().length - 1; index >= 0; index--) {
+
+        }
+
+        return -1;
     }
 }
