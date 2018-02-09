ff46eecb8e67e08f63e8ed9e0ad0f0034431398b
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-02-08 23:36:48 -0800

    Add inc/dec vCPUs in DCORAL algorithm class

--- src/DecisionMaker/DCORALAlgorithm.java
+++ src/DecisionMaker/DCORALAlgorithm.java
@@ -1,4 +1,17 @@
 package DecisionMaker;
 
+import Beans.ControllerBean;
+import Scaling.CPUScaling;
+
 public class DCORALAlgorithm extends AbstractDecisionMaker implements DecisionMaker {
+
+    public void incVirtualCPUs (int numCPUs, ControllerBean controller) {
+        CPUScaling cpuScaling = new CPUScaling();
+        cpuScaling.incMultipleVirtualCPUs(numCPUs, controller);
+    }
+
+    public void decVirtualCPUs (int numCPUs, ControllerBean controller) {
+        CPUScaling cpuScaling = new CPUScaling();
+        cpuScaling.decMultipleVirtaulCPUs(numCPUs, controller);
+    }
 }
