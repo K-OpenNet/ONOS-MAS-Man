e4cdbd6f8573910d5b6d80dfa994e226fac7308a
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-08 14:16:53 -0800

    [CARD-61] multi-thread-based function to change multiple master controller

--- src/Mastership/AbstractMastership.java
+++ src/Mastership/AbstractMastership.java
@@ -33,11 +33,51 @@ abstract class AbstractMastership implements Mastership{
 
     public void changeMultipleMastership(HashMap<String, ArrayList<String>> topology) {
 
+        ArrayList<Thread> threads = new ArrayList<>();
+
         for (String controllerId : topology.keySet()) {
-            ControllerBean tmpControllerBean = Configuration.getInstance().getControllerBeanWithId(controllerId);
-            for (String dpid : topology.get(controllerId)) {
-                changeMastership(dpid, tmpControllerBean);
+            ThreadChangeMultipleMastership runnableObj = new ThreadChangeMultipleMastership(controllerId, topology.get(controllerId));
+            Thread thread = new Thread(runnableObj);
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
+}
+
+class ThreadChangeMultipleMastership implements Runnable {
+
+    private String controllerId;
+    private ArrayList<String> switches;
+
+    public ThreadChangeMultipleMastership(String controllerId, ArrayList<String> switches) {
+        this.controllerId = controllerId;
+        this.switches = switches;
+    }
+
+    @Override
+    public void run() {
+        ControllerBean tmpControllerBean = Configuration.getInstance().getControllerBeanWithId(controllerId);
+        for (String dpid : switches) {
+            changeMastership(dpid, tmpControllerBean);
+        }
+    }
+
+    public void changeMastership(String dpid, ControllerBean targetController) {
+        JsonObject rootObj = new JsonObject();
+        rootObj.add("deviceId", dpid);
+        rootObj.add("nodeId", targetController.getControllerId());
+        rootObj.add("role", "MASTER");
+
+        RESTConnection restConn = new RESTConnection();
+        restConn.putCommandToUser(targetController, RESTURL_DOMASTERSHIP, rootObj);
+    }
+
 }
--- src/Mastership/Mastership.java
+++ src/Mastership/Mastership.java
@@ -1,9 +1,12 @@
 package Mastership;
 
+import Beans.ControllerBean;
+
 public interface Mastership {
     enum mastershipType {
         EQUALMASTERSHIP, CPMAN;
     }
 
     void runMastershipAlgorithm();
+    void changeMastership(String dpid, ControllerBean targetController);
 }
