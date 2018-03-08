f0005dd738fc6b64780fa3ba006d003fcee4219c
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-08 13:59:31 -0800

    Bugfix: miss commands to send Json to ONOS

--- src/Mastership/AbstractMastership.java
+++ src/Mastership/AbstractMastership.java
@@ -2,10 +2,13 @@ package Mastership;
 
 import Beans.ControllerBean;
 import Database.Configure.Configuration;
+import Utils.Connection.RESTConnection;
 import com.eclipsesource.json.JsonObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static Database.Configure.Configuration.RESTURL_DOMASTERSHIP;
 
 abstract class AbstractMastership implements Mastership{
     protected mastershipType mastershipName;
@@ -23,6 +26,9 @@ abstract class AbstractMastership implements Mastership{
         rootObj.add("deviceId", dpid);
         rootObj.add("nodeId", targetController.getControllerId());
         rootObj.add("role", "MASTER");
+
+        RESTConnection restConn = new RESTConnection();
+        restConn.putCommandToUser(targetController, RESTURL_DOMASTERSHIP, rootObj);
     }
 
     public void changeMultipleMastership(HashMap<String, ArrayList<String>> topology) {
