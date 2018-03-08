13ae612f71857c0d4771e62e48dd5a8c48e4d6bd
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-07 17:45:01 -0800

    [CARD-47] change mastership functions in abstract class

--- src/Mastership/AbstractMastership.java
+++ src/Mastership/AbstractMastership.java
@@ -1,5 +1,9 @@
 package Mastership;
 
+import Beans.ControllerBean;
+import Beans.SwitchBean;
+import com.eclipsesource.json.JsonObject;
+
 abstract class AbstractMastership implements Mastership{
     protected mastershipType mastershipName;
 
@@ -9,5 +13,12 @@ abstract class AbstractMastership implements Mastership{
 
     public void setMastershipName(mastershipType mastershipName) {
         this.mastershipName = mastershipName;
+    }
+
+    public void changeMastership(SwitchBean sw, ControllerBean targetController) {
+        JsonObject rootObj = new JsonObject();
+        rootObj.add("deviceId", sw.getDpid());
+        rootObj.add("nodeId", targetController.getControllerId());
+        rootObj.add("role", "MASTER");
     }
 }
