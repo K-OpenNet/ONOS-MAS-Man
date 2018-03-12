eb2fe4d6a8af24bfe0f5cb8d720b15af35b7aeb4
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-12 11:20:04 -0700

    [CARD-63] Add noscaling algorithm as a class

---
+++ src/DecisionMaker/NoScalingCPManAlgorithm.java
@@ -0,0 +1,7 @@
+package DecisionMaker;
+
+public class NoScalingCPManAlgorithm extends AbstractDecisionMaker implements DecisionMaker {
+    public NoScalingCPManAlgorithm() {
+        decisionMakerName = decisionMakerType.NOSCALING_CPMAN;
+    }
+}
