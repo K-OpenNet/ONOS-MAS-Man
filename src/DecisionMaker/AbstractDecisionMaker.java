95b7acdaecae5445ce43dcf7bcbdd2c12bcc7486
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-12 14:05:06 -0700

    delete states in decision maker

--- src/DecisionMaker/AbstractDecisionMaker.java
+++ src/DecisionMaker/AbstractDecisionMaker.java
@@ -1,12 +1,8 @@
 package DecisionMaker;
 
-import Database.Tables.State;
-
-import java.util.ArrayList;
 
 abstract class AbstractDecisionMaker implements DecisionMaker {
     protected decisionMakerType decisionMakerName;
-    protected ArrayList<State> currentDB;
 
     public decisionMakerType getDecisionMakerName() {
         return decisionMakerName;
@@ -16,11 +12,4 @@ abstract class AbstractDecisionMaker implements DecisionMaker {
         this.decisionMakerName = decisionMakerName;
     }
 
-    public ArrayList<State> getCurrentDB() {
-        return currentDB;
-    }
-
-    public void setCurrentDB(ArrayList<State> currentDB) {
-        this.currentDB = currentDB;
-    }
 }
