b99032de757467e83726657815a0a93a561d7847
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-12 14:02:39 -0700

    Add state list from DB in each decision maker

--- src/DecisionMaker/AbstractDecisionMaker.java
+++ src/DecisionMaker/AbstractDecisionMaker.java
@@ -1,7 +1,12 @@
 package DecisionMaker;
 
+import Database.Tables.State;
+
+import java.util.ArrayList;
+
 abstract class AbstractDecisionMaker implements DecisionMaker {
     protected decisionMakerType decisionMakerName;
+    protected ArrayList<State> currentDB;
 
     public decisionMakerType getDecisionMakerName() {
         return decisionMakerName;
@@ -9,5 +14,13 @@ abstract class AbstractDecisionMaker implements DecisionMaker {
 
     public void setDecisionMakerName(decisionMakerType decisionMakerName) {
         this.decisionMakerName = decisionMakerName;
+    }
+
+    public ArrayList<State> getCurrentDB() {
+        return currentDB;
+    }
+
+    public void setCurrentDB(ArrayList<State> currentDB) {
+        this.currentDB = currentDB;
     }
 }
