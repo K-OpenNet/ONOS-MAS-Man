37fe26320b265025c78662d4d2e5c03092639d32
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-12 14:00:51 -0700

    [CARD-63] Implement NoScaling class

--- src/DecisionMaker/NoScalingCPManAlgorithm.java
+++ src/DecisionMaker/NoScalingCPManAlgorithm.java
@@ -1,5 +1,8 @@
 package DecisionMaker;
 
+import Database.Configure.Configuration;
+import Mastership.CPManMastership;
+
 public class NoScalingCPManAlgorithm extends AbstractDecisionMaker implements DecisionMaker {
     public NoScalingCPManAlgorithm() {
         decisionMakerName = decisionMakerType.NOSCALING_CPMAN;
@@ -8,5 +11,14 @@ public class NoScalingCPManAlgorithm extends AbstractDecisionMaker implements De
     @Override
     public void runDecisionMakerAlgorithm(int currentTimeIndex) {
 
+        if (currentTimeIndex % Configuration.NOSCALING_CPMAN_PERIOD == 0) {
+            runCPManMastershipAlgorithm();
+        }
+
+    }
+
+    public void runCPManMastershipAlgorithm() {
+        CPManMastership mastership = new CPManMastership();
+        mastership.runMastershipAlgorithm();
     }
 }
