261cfcf4cf517b595949a1ed2ac7f4a1dca04c5d
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-27 13:02:51 +0900

    [CARD-46] add functions to get active, undersub, and oversub controllers

--- src/Mastership/CPManMastership.java
+++ src/Mastership/CPManMastership.java
@@ -2,6 +2,9 @@ package Mastership;
 
 import Database.Tables.State;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CPManMastership extends AbstractMastership implements Mastership {
     public CPManMastership() {
         mastershipName = mastershipType.CPMAN;
@@ -9,7 +12,31 @@ public class CPManMastership extends AbstractMastership implements Mastership {
 
     @Override
     public void runMastershipAlgorithm(State state) {
+        HashMap<String, ArrayList<String>> topology = new HashMap<>();
 
+        ArrayList<String> activeControllers = getActiveControllers();
+        ArrayList<String> underSubControllers = getUnderSubControllers();
+        ArrayList<String> overSubControllers = getOverSubControllers();
+
+        changeMultipleMastership(topology);
+    }
+
+    public ArrayList<String> getActiveControllers() {
+        ArrayList<String> activeControllers = new ArrayList<>();
+
+        return activeControllers;
+    }
+
+    public ArrayList<String> getUnderSubControllers() {
+        ArrayList<String> underSubControllers = new ArrayList<>();
+
+        return underSubControllers;
+    }
+
+    public ArrayList<String> getOverSubControllers() {
+        ArrayList<String> overSubControllers = new ArrayList<>();
+
+        return overSubControllers;
     }
 
 }
