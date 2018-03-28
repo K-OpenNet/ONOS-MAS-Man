ba257bc76b06e2fef95167635dac5976b5c3c807
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-28 14:01:28 +0900

    [CARD-55] test code: remove to print information in CPMan loop

--- src/Mastership/CPManMastership.java
+++ src/Mastership/CPManMastership.java
@@ -66,11 +66,11 @@ public class CPManMastership extends AbstractMastership implements Mastership {
                 long tmpMostControllerOFMsgsChanged = tmpMostControllerOFMsgs - tmpSwitchOFMsgs;
                 long tmpLeastControllerOFMsgsChanged = tmpLeastControllerOFMsgs + tmpSwitchOFMsgs;
                 // test code
-                System.out.println("@@@@@ average: " + avgNumOFMsgs);
-                System.out.println("@@@@@ " + tmpDpid + ": " + tmpSwitchOFMsgs);
-                System.out.println("@@@@@ Most - " + mostController.getControllerId() + ": " + tmpMostControllerOFMsgs + ", " + tmpMostControllerOFMsgsChanged);
-                System.out.println("@@@@@ Least - " + leastController.getControllerId() + ": " + tmpLeastControllerOFMsgs + ", " + tmpLeastControllerOFMsgsChanged);
-                System.out.println();
+                //System.out.println("@@@@@ average: " + avgNumOFMsgs);
+                //System.out.println("@@@@@ " + tmpDpid + ": " + tmpSwitchOFMsgs);
+                //System.out.println("@@@@@ Most - " + mostController.getControllerId() + ": " + tmpMostControllerOFMsgs + ", " + tmpMostControllerOFMsgsChanged);
+                //System.out.println("@@@@@ Least - " + leastController.getControllerId() + ": " + tmpLeastControllerOFMsgs + ", " + tmpLeastControllerOFMsgsChanged);
+                //System.out.println();
 
                 // can this switch be moved?
                 if (tmpMostControllerOFMsgsChanged > avgNumOFMsgs && tmpLeastControllerOFMsgsChanged < avgNumOFMsgs) {
