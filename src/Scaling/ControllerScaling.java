737858a6d7f58c76f7c7a2d656562119f2e7e677
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-03-28 15:12:05 +0900

    [CARD-29,30,31] scaffolding L1-L3 scaling function in ControllerScaling class

--- src/Scaling/ControllerScaling.java
+++ src/Scaling/ControllerScaling.java
@@ -1,8 +1,28 @@
 package Scaling;
 
+import Controller.Controller;
+
 public class ControllerScaling extends AbstractScaling implements Scaling {
 
     public ControllerScaling() {
         scalingName = scalingType.CONTROLLERSCALING;
+    }
+
+    public void runL1ONOSScaleIn() {
+    }
+
+    public void runL2ONOSScaleIn() {
+    }
+
+    public void runL3ONOSScaleIn() {
+    }
+
+    public void runL1ONOSScaleOut() {
+    }
+
+    public void runL2ONOSScaleOut() {
+    }
+
+    public void runL3ONOSScaleOut() {
     }
 }
