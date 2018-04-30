55ae60f56673bcdb925fe917e0002956120b5528
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-04-30 19:03:43 +0900

    [CARD-97] reduce scaling time 3

--- src/Mastership/AbstractMastership.java
+++ src/Mastership/AbstractMastership.java
@@ -100,11 +100,11 @@ class ThreadChangeSingleMastership implements Runnable {
 
         changeMastership(dpid, controllerId);
         while (!verifyMastership(dpid, controllerId)) {
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
