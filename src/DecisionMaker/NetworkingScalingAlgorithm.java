8e33a391939ef8e70d4b9c771a04340580bfc97e
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-05-02 13:22:40 +0900

    [CARD-87] Scaffolding Elastic algorithm based on network usage

--- src/DecisionMaker/NetworkingScalingAlgorithm.java
+++ src/DecisionMaker/NetworkingScalingAlgorithm.java
@@ -1,9 +1,14 @@
 package DecisionMaker;
 
+import Beans.ControllerBean;
+import Database.Configure.Configuration;
 import Database.Tables.State;
 import Mastership.CPManMastership;
+import Scaling.ControllerScaling;
 
 import java.util.ArrayList;
+
+import static Database.Configure.Configuration.SCALING_LEVEL;
 
 public class NetworkingScalingAlgorithm extends AbstractDecisionMaker implements DecisionMaker {
     public NetworkingScalingAlgorithm() {
@@ -13,10 +18,85 @@ public class NetworkingScalingAlgorithm extends AbstractDecisionMaker implements
     @Override
     public void runDecisionMakerAlgorithm(int currentTimeIndex, ArrayList<State> dbDump) {
 
+        if (currentTimeIndex == 0) {
+            return;
+        }
+
+        ArrayList<State> targetStates = new ArrayList<>();
+
+        int startPoint = currentTimeIndex - Configuration.NOSCALING_CPMAN_PERIOD + 1;
+        int endPoint = currentTimeIndex;
+
+        for (int index = startPoint; index <= endPoint; index++) {
+            targetStates.add(dbDump.get(index));
+        }
+
+        State state = mergeStates(targetStates);
+
+        boolean scaleInFlag = false;
+        boolean scaleOutFlag = false;
+
+        ControllerBean targetControllerScaleIn = null;
+        ControllerBean targetControllerScaleOut = null;
     }
 
     public void runCPManMastershipAlgorithm(State state) {
         CPManMastership mastership = new CPManMastership();
         mastership.runMastershipAlgorithm(state);
+    }
+
+    public void runBalancingOnly(State state) {
+        runCPManMastershipAlgorithm(state);
+    }
+
+    public void runScaleIn(ControllerBean targetController, State state) {
+        CPManMastership mastership = new CPManMastership();
+        ControllerScaling scaling = new ControllerScaling();
+
+        if (mastership.getActiveControllers().size() == 3) {
+            runCPManMastershipAlgorithm(state);
+            return;
+        }
+
+        switch(SCALING_LEVEL) {
+            case 1:
+                scaling.runL1ONOSScaleIn(targetController, state);
+                break;
+            case 2:
+                scaling.runL2ONOSScaleIn(targetController, state);
+                break;
+            case 3:
+                scaling.runL3ONOSScaleIn(targetController, state);
+                break;
+            default:
+                throw new WrongScalingLevelException();
+        }
+
+    }
+
+    public void runScaleOut(ControllerBean targetController, State state) {
+        CPManMastership mastership = new CPManMastership();
+        ControllerScaling scaling = new ControllerScaling();
+
+        // Maximum number of controllers
+        if (mastership.getActiveControllers().size() == Configuration.getInstance().getControllers().size()) {
+            runCPManMastershipAlgorithm(state);
+            return;
+        }
+
+
+        switch(SCALING_LEVEL) {
+            case 1:
+                scaling.runL1ONOSScaleOut(targetController, state);
+                break;
+            case 2:
+                scaling.runL2ONOSScaleOut(targetController, state);
+                break;
+            case 3:
+                scaling.runL3ONOSScaleOut(targetController, state);
+                break;
+            default:
+                throw new WrongScalingLevelException();
+        }
     }
 }
