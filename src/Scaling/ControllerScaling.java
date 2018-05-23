06774e56656f2e652f9e061fc5c00e487b06052a
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-05-23 18:00:36 +0900

    Change waiting time

--- src/Scaling/ControllerScaling.java
+++ src/Scaling/ControllerScaling.java
@@ -551,7 +551,7 @@ public class ControllerScaling extends AbstractScaling implements Scaling {
 //        }
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -560,7 +560,7 @@ public class ControllerScaling extends AbstractScaling implements Scaling {
         addControllerToOVS(targetController, state);
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
