e1987e0bf6d4a2984a83254a81b5d0f74c891e47
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-10-16 19:53:44 +0900

    [hecp-4]Scaffolding H-ECP class

---
+++ src/DecisionMaker/HybridECP.java
@@ -0,0 +1,12 @@
+package DecisionMaker;
+
+import Database.Tables.State;
+
+import java.util.ArrayList;
+
+public class HybridECP extends AbstractDecisionMaker implements DecisionMaker {
+    @Override
+    public void runDecisionMakerAlgorithm(int currentTimeIndex, ArrayList<State> dbDump) {
+
+    }
+}
