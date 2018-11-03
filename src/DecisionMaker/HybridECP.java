53a1bb14159e7d208658c342d4eb6b52be7e24a1
Woojoong Kim <woojoong.m.kim@gmail.com>
2018-11-03 16:10:55 +0900

    [hecp] mark last scaloue time index

--- src/DecisionMaker/HybridECP.java
+++ src/DecisionMaker/HybridECP.java
@@ -457,6 +457,7 @@ class ThreadLane2Algorithm implements Runnable {
         dt = new Date();
         System.out.println("** L2 Scaling time: " + (dt.getTime() - startTime) + " (timeslot: " + Controller.getTimeIndex() + ", Algorithm: " + "HECP" + ")");
         System.out.println("*** End L2 algorithm");
+        LAST_SCALEOUT_TIME_INDEX = Controller.getTimeIndex();
     }
 
     public void switchOnMultipleControllers(int numTargetControllers, State state) {
